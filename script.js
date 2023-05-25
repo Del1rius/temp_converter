ecae9f645345508567fc3b36ab76896225c162b2
Del1rius <timothy.aidan.barry@gmail.com>
2023-05-25 09:54:01 +0200

    temp converter finished

---
+++ script.js
@@ -0,0 +1,15 @@
+let celsius = document.getElementById("celsius");
+let fahrenheit = document.getElementById("fahrenheit");
+
+function celsiusToFar() {
+    let output = (parseFloat(celsius.value) * 9/5) + 32;
+    fahrenheit.value = parseFloat(output.toFixed(2));
+
+}
+
+function farToCelsius() {
+     let output = (parseFloat(fahrenheit.value) -32) * 5/9;
+     celsius.value = parseFloat(output.toFixed(2));
+}
+
+function reset ()
